d64ce8fc15033b6d899c18850b2ebd39b02350b7
ilyaminkin <ilya.minkinen@gmail.com>
2012-05-17 11:04:52 +0400

    Graph simplification is rewritten

--- src/graphalgorithm.cpp
+++ src/graphalgorithm.cpp
@@ -5,7 +5,8 @@ namespace SyntenyBuilder
 {
 	namespace
 	{
-		int pos;
+		int deletedWhirl;
+		int deletedBulge;
 		typedef char Bool;
 
 		class HashFunction
@@ -19,16 +20,21 @@ namespace SyntenyBuilder
 
 		struct VisitData
 		{
-			int classId;
-			int instance;
+			char classId;
+			DeBruijnGraph::Edge * edge;
 			int distance;
-
 			VisitData() {}
-			VisitData(int classId, int instance, int distance):
-				classId(classId), instance(instance), distance(distance) {}
+			VisitData(char classId, DeBruijnGraph::Edge * edge, int distance):
+				classId(classId), edge(edge), distance(distance) {}
+
+			bool operator < (const VisitData & toCompare)
+			{
+				return std::make_pair(classId, distance) < std::make_pair(toCompare.classId, toCompare.distance);
+			}
 		};
 
-		typedef google::dense_hash_set<DeBruijnGraph::Vertex, HashFunction> VertexVisit;
+		const int ANY_CLASS = -1;		
+		typedef google::dense_hash_set<DeBruijnGraph::Vertex, HashFunction> VertexVisit;		
 		typedef google::dense_hash_map<DeBruijnGraph::Vertex, std::vector<VisitData>, HashFunction> VertexVisitMap;
 
 		std::ostream& operator << (std::ostream & out, const DeBruijnGraph::Vertex & v)
@@ -185,293 +191,188 @@ namespace SyntenyBuilder
 			CopyN(e.StartIterator(), size, std::ostream_iterator<char>(out));
 			std::cerr << std::endl;
 		}
-
-		void FindParallelEdges(DeBruijnGraph & g, DeBruijnGraph::Edge edge, int size, 
-			std::vector<DeBruijnGraph::Edge> & ret)
+		
+		void CollapseBulge(DeBruijnGraph & g, const VisitData & source, const VisitData & target)
 		{
-			ret.clear();
-			std::vector<DeBruijnGraph::Edge> init;
-			g.FindEquivalentEdges(edge, init);
-			std::vector<DeBruijnGraph::Edge> now(init);
-
+			
 		#ifdef _DEBUG
-			std::cerr << "Path: ";
-			PrintPath(edge, size + g.EdgeSize(), std::cerr);
-			std::cerr << "Finding parallel paths, candidates: " << std::endl;
-			for(size_t i = 0; i < init.size(); i++)
-			{
-				bool suits = true;
-				DeBruijnGraph::Edge test = init[i];
-				for(int j = 0; j < size; j++)
-				{
-					if(test.IsNull())
-					{
-						suits = false;
-						break;
-					}
-
-					MoveForward(test);
-				}
-
-				if(suits && !test.IsNull())
-				{
-					PrintPath(init[i], size + g.EdgeSize(), std::cerr);
-				}
-			}
-
-			std::cerr << "---------------" << std::endl;
-		#endif
-
-			for(int step = 0; step < size; step++)
-			{
-				MoveForward(edge);
-				for(size_t i = 0; i < now.size(); i++)
-				{	
-					if(!now[i].IsNull())
-					{
-						MoveForward(now[i]);
-					}
-
-					if(now[i].IsNull() || now[i].EndVertex() != edge.EndVertex())
-					{
-						init[i] = DeBruijnGraph::Edge();
-					}					
-				}								
-			}
-						
-			std::copy(init.begin(), std::remove(init.begin(), init.end(), DeBruijnGraph::Edge()),
-				std::back_inserter(ret));
-		}
-
-		bool CheckBulge(std::vector<DeBruijnGraph::Edge> edge1, int size1, std::vector<DeBruijnGraph::Edge> edge2, int size2)
-		{
-			std::vector<int> visit;
-			for(int i = 0; i < size1; i++)
-			{
-				for(size_t j = 0; j < edge1.size(); j++)
-				{					
-					visit.push_back(edge1[j].Position());
-					MoveForward(edge1[j]);
-				}
-			}
-
-			std::sort(visit.begin(), visit.end());
-			for(int i = 0; i < size2; i++)
-			{
-				for(size_t j = 0; j < edge2.size(); j++)
-				{
-					if(std::binary_search(visit.begin(), visit.end(), edge2[j].Position()))
-					{
-						std::cerr << "Fail = " << edge2[j].Position() << std::endl;
-						return false;
-					}
-
-					MoveForward(edge2[j]);
-				}
-			}
-
-			return true;
-		}
-
-		bool CheckBranch(std::vector<DeBruijnGraph::Edge> edge1, int size1)
-		{
-			std::vector<int> visit;
-			for(int i = 0; i < size1; i++)
-			{
-				for(size_t j = 0; j < edge1.size(); j++)
-				{					
-					visit.push_back(edge1[j].Position());
-					MoveForward(edge1[j]);
-				}
-			}
-
-			std::sort(visit.begin(), visit.end());
-			for(size_t i = 0; visit.size() > 0 && i < visit.size() - 1; i++)
-			{
-				if(visit[i] == visit[i + 1])
-				{
-					std::cerr << "Fail2 = " << visit[i] << ", " << visit[i + 1] << std::endl;
-					return false;
-				}
-			}
-
-			return true;
-		}
-
-		bool CollapseBulge(DeBruijnGraph & g,
-			const DeBruijnGraph::Vertex & start,
-			const DeBruijnGraph::Vertex & dest, 
-			DeBruijnGraph::Edge smallBranch, int smallSize,
-			DeBruijnGraph::Edge bigBranch, int bigSize)
-		{			
-			std::vector<DeBruijnGraph::Edge> parallel;
-			std::vector<DeBruijnGraph::Edge> smallParallel;
-			FindParallelEdges(g, bigBranch, bigSize, parallel);
-			FindParallelEdges(g, smallBranch, smallSize, smallParallel);
-			std::cerr << "Pos = " << pos << ", Instances = " << parallel.size() + smallParallel.size() << std::endl;
-
-			bool bigCheck = CheckBranch(parallel, bigSize);
-			bool smallCheck = CheckBranch(smallParallel, smallSize);			
-
-		#ifndef _DEBUG
 			static int bulge = 0;
 			std::cerr << "Bulge #" << bulge++ << std::endl;
 			std::cerr << "Before: " << std::endl;
-		//	PrintRaw(g.sequence, std::cerr);
-			std::cerr << "Small branch: " << std::endl;
-			for(size_t i = 0; i < smallParallel.size(); i++)
-			{
-				PrintPath(smallParallel[i], smallSize + g.EdgeSize(), std::cerr);
-			}
-			
-			std::cerr << "Big branch: " << std::endl;
-			for(size_t i = 0; i < parallel.size(); i++)
-			{
-				PrintPath(parallel[i], bigSize + g.EdgeSize(), std::cerr);
-			}
-
+			PrintRaw(g.sequence, std::cerr);
+			std::cerr << "Source branch: " << std::endl;			
+			int shift = source.distance == 0 ? 1 : 0;
+			PrintPath(*source.edge, source.distance + g.EdgeSize() - shift, std::cerr);
+			std::cerr << "Target branch: " << std::endl;			
+			PrintPath(*target.edge, target.distance + g.EdgeSize(), std::cerr);
+			std::cerr << std::endl;
 		#endif
-		
-			if(!bigCheck && !smallCheck)
+			
+			DNASequence::StrandIterator it = --source.edge->EndIterator();
+			DNASequence::StrandIterator jt = --target.edge->EndIterator();
+			for(int i = 0; i < source.distance; i++, ++it, ++jt)
 			{
-				return false;
+				it.AssignBase(*jt);
 			}
 
-			if(!bigCheck)
+			int diff = target.distance - source.distance;
+			for(int i = 0; i < diff; i++)
 			{
-				if(smallSize == bigSize)
+				deletedBulge++;
+				jt.Invalidate();				
+			}
+
+		#ifdef _DEBUG
+			std::cerr << std::endl << "After: " << std::endl;
+			PrintRaw(g.sequence, std::cerr);
+			std::cerr << "Source branch: " << std::endl;			
+			PrintPath(*source.edge, source.distance + g.EdgeSize() - shift, std::cerr);
+			std::cerr << "Target branch: " << std::endl;			
+			PrintPath(*target.edge, source.distance + g.EdgeSize() - shift, std::cerr);
+			std::cerr << std::endl << std::string(80, '-') << std::endl;
+		#endif
+		}
+
+		int ProcessVertex(DeBruijnGraph & g, VertexVisitMap::iterator it, 
+			std::map<DeBruijnGraph::Edge*, int> & deprecate)
+		{
+			int ret = 0;
+			int source = -1;				
+			for(int i = 0; i < static_cast<int>(it->second.size()); i++)
+			{
+				if(deprecate[it->second[i].edge] <= it->second[i].distance)
 				{
-					return CollapseBulge(g, start, dest, bigBranch, bigSize, smallBranch, smallSize);
-				}
-
-				return false;
-			}
-
-			if(!CheckBulge(smallParallel, smallSize, parallel, bigSize))
-			{
-				return false;
-			}
-
-			std::vector<DeBruijnGraph::StrandIterator> it;
-			for(size_t i = 0; i < parallel.size(); i++)
-			{
-				it.push_back(--parallel[i].EndIterator());
-			}
-
-			for(int step = 0; step < smallSize; step++)
-			{
-				for(size_t i = 0; i < it.size(); i++)
-				{
-					it[i].AssignBase(smallBranch.SpellEnd());
-					++it[i];
-				}
-
-				MoveForward(smallBranch);
-			}
-
-			for(int step = 0; step < bigSize - smallSize; step++)
-			{
-				for(size_t i = 0; i < it.size(); i++)
-				{
-					it[i].Invalidate();
+					source = i;
+					break;
 				}
 			}
-		
-			return true;
-		}		
 
-		int ProcessBifurcation(DeBruijnGraph & g, DeBruijnGraph::Vertex & v, int minCycleSize)
+			if(source >= 0)
+			{
+				char classId = it->second[source].classId;
+				for(int i = source + 1; i < static_cast<int>(it->second.size()); i++)
+				{
+					if(deprecate[it->second[i].edge] <= it->second[i].distance &&
+						it->second[i].classId != classId)
+					{
+						ret++;
+						CollapseBulge(g, it->second[source], it->second[i]);
+						deprecate[it->second[i].edge] = it->second[i].distance;
+					}
+				}
+			}
+
+			return ret;
+		}
+		void CollapseWhirl(DeBruijnGraph & g, DeBruijnGraph::Edge & e, int distance)
+		{
+
+		#ifdef _DEBUG
+			static int whirl = 0;
+			std::cerr << "Whirl #" << whirl++ << std::endl;
+			std::cerr << "Before: " << std::endl;
+			PrintRaw(g.sequence, std::cerr);
+			std::cerr << "Whirl branch: " << std::endl;			
+			PrintPath(e, distance + g.EdgeSize(), std::cerr);	
+			std::cerr << std::endl;
+		#endif
+			DNASequence::StrandIterator it = --e.EndIterator();
+			for(int i = 0; i <= distance; i++)
+			{
+				deletedWhirl++;
+				it.Invalidate();
+			}
+
+		#ifdef _DEBUG
+			std::cerr << std::endl << "After: " << std::endl;
+			PrintRaw(g.sequence, std::cerr);
+			std::cerr << std::endl << std::string(80, '-') << std::endl;
+		#endif
+		}
+
+		int RemoveWhirls(DeBruijnGraph & g, DeBruijnGraph::Vertex & v, int minCycleSize)
+		{
+			int ret = 0;
+			bool keepOn = true;
+			std::set<int> deprecate;
+			std::vector<DeBruijnGraph::Edge> now;
+			std::vector<DeBruijnGraph::Edge> start;			
+			int iter = 0;
+			while(keepOn)
+			{
+				keepOn = false;
+				deprecate.clear();
+				g.ListEdges(v, start);
+				now = start;
+				for(int step = 0; step < minCycleSize; step++)
+				{
+					for(size_t i = 0; i < static_cast<int>(now.size()); i++)
+					{
+						if(!now[i].IsNull() && deprecate.find(now[i].Position()) == deprecate.end())
+						{
+							DeBruijnGraph::Vertex u = now[i].EndVertex();
+							if(u == v)
+							{
+								ret++;
+								keepOn = true;
+								MoveForward(now[i]);
+								if(!now[i].IsNull())
+								{
+									deprecate.insert(now[i].Position());
+								}
+
+								CollapseWhirl(g, start[i], step);
+							}						
+							else
+							{
+								MoveForward(now[i]);
+							}
+						}
+					}
+				}
+			}
+
+			return ret;
+		}
+
+		int RemoveBulges(DeBruijnGraph & g, DeBruijnGraph::Vertex & v, int minCycleSize)
 		{			
 			int ret = 0;
 			VertexVisitMap visit;
-			visit.set_empty_key(DeBruijnGraph::Vertex());			
-			std::vector<std::vector<DeBruijnGraph::Edge> > init;
-			g.ListEdgesSeparate(v, init);
-			std::vector<char> alive(init.size(), 1);
-			std::vector<std::vector<DeBruijnGraph::Edge> > now = init;
-			bool keepOn = true;
+			visit.set_empty_key(DeBruijnGraph::Vertex());
+			std::map<DeBruijnGraph::Edge*, int> deprecate;
 
-			for(int step = 1; step <= minCycleSize && keepOn; step++)
+			std::vector<std::vector<DeBruijnGraph::Edge> > now;
+			std::vector<std::vector<DeBruijnGraph::Edge> > start;
+			g.ListEdgesSeparate(v, start);
+			now = start;
+			for(int classId = 0; classId < static_cast<int>(now.size()); classId++)
 			{
+				for(int instance = 0; instance < static_cast<int>(now[classId].size()); instance++)
+				{
+					deprecate[&now[classId][instance]] = -1;
+				}
+			}
 
-			#ifdef _DEBUG
-				std::cerr << "step = " << step << std::endl;
-			#endif			
-
-				keepOn = false;
+			for(int step = 0; step < minCycleSize; step++)
+			{
 				for(int classId = 0; classId < static_cast<int>(now.size()); classId++)
 				{
-					if(!alive[classId])
-					{
-						continue;
-					}
-
 					for(int instance = 0; instance < static_cast<int>(now[classId].size()); instance++)
 					{
-						if(now[classId][instance].IsNull() || !alive[classId])
+						if(!now[classId][instance].IsNull())
 						{
-							continue;
+							DeBruijnGraph::Vertex u = now[classId][instance].EndVertex();
+							visit[u].push_back(VisitData(classId, &start[classId][instance], step));
+							MoveForward(now[classId][instance]);
 						}
-
-						keepOn = true;
-						DeBruijnGraph::Vertex u = now[classId][instance].EndVertex();
-
-					#ifdef _DEBUG
-						u.Spell(std::ostream_iterator<char>(std::cerr));
-						std::cerr << std::endl;
-					#endif
-
-						VertexVisitMap::iterator it = visit.find(u);
-						if(it == visit.end())
-						{
-							visit.insert(std::make_pair(u, std::vector<VisitData>(1, VisitData(classId, instance, step))));
-						}
-						else 
-						{
-							DeBruijnGraph::Edge & nowEdge = init[classId][instance];
-							for(size_t i = 0; i < it->second.size(); i++)
-							{
-								if(it->second[i].classId != classId)
-								{									
-									int prevStep = it->second[i].distance;
-									int size = step + prevStep;
-									DeBruijnGraph::Edge & prevEdge = init[it->second[i].classId][it->second[i].instance];									
-									if(size > 2 && size <= minCycleSize)
-									{			
-										if(step < it->second[i].distance)
-										{
-											if(CollapseBulge(g, v, u, nowEdge, step - 1, prevEdge, prevStep - 1))
-											{
-												alive[it->second[i].classId] = 0;
-												ret++;
-											}
-										}
-										else
-										{
-											if(CollapseBulge(g, v, u, prevEdge, prevStep - 1, nowEdge, step - 1))
-											{
-												alive[classId] = 0;
-												ret++;
-												break;
-											}
-										}										
-									}
-									else
-									{
-										it->second.push_back(VisitData(classId, instance, step));
-									}
-								}
-							}
-						}
-
-						MoveForward(now[classId][instance]);
 					}
 				}
+			}
 
-			#ifdef _DEBUG
-				std::cerr << "---------------" << std::endl;
-			#endif
-
+			for(VertexVisitMap::iterator it = visit.begin(); it != visit.end(); it++)
+			{
+				ret += ProcessVertex(g, it, deprecate);
 			}
 
 			return ret;
@@ -555,25 +456,32 @@ namespace SyntenyBuilder
 		std::cerr << std::endl;
 	#endif
 		int bulgeCount = 0;
+		int whirlCount = 0;
 		int bifurcationCount = 0;
+		deletedWhirl = deletedBulge = 0;
  		g.sequence.KeepHash(g.VertexSize());
 		std::vector<std::vector<DeBruijnGraph::Edge> > edge;
 		for(DeBruijnGraph::StrandConstIterator it = g.sequence.PositiveBegin(); it.Valid(); it++)
 		{
-			if(it.GetPosition() % 1 == 0)
+			if(it.GetPosition() % 10000 == 0)
 			{
-				std::cerr << it.GetPosition() << " " << bifurcationCount << " " << bulgeCount << std::endl;
+				std::cerr << it.GetPosition() << " " << bifurcationCount << " " << whirlCount << ' ' << bulgeCount << std::endl;
 			}
 
-			pos = it.GetPosition();
 			DeBruijnGraph::Vertex v = g.ConstructVertex(it);
 			if(!v.IsNull() && g.ListEdgesSeparate(v, edge) > 1)
 			{
 				bifurcationCount++;
-				bulgeCount += ProcessBifurcation(g, v, minCycleSize);
+				whirlCount += RemoveWhirls(g, v, minCycleSize);
+				bulgeCount += RemoveBulges(g, v, minCycleSize);
 			}
 		}
 
+		std::cerr << "Total bifurcations: " << bifurcationCount << std::endl;
+		std::cerr << "Deleted bpairs by whirl removal: " << deletedWhirl << std::endl;
+		std::cerr << "Deleted bpairs by bulge removal: " << deletedBulge << std::endl;
+		std::cerr << "Total whirls: " << whirlCount << std::endl;
+		std::cerr << "Total bulges: " << bulgeCount << std::endl;		
 		g.sequence.Optimize();
 		return bulgeCount;
 	}
