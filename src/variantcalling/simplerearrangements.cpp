74a4194a0897d2582741c9afca15ff83c81d9810
Ilya Minkin <ilya.minkinen@gmail.com>
2013-04-13 14:09:51 +0400

    Added missing copyright

--- src/variantcalling/simplerearrangements.cpp
+++ src/variantcalling/simplerearrangements.cpp
@@ -1,3 +1,10 @@
+//****************************************************************************
+//* Copyright (c) 2012 Saint-Petersburg Academic University
+//* All Rights Reserved
+//* See file LICENSE for details.
+//****************************************************************************
+
+
 #include <iostream>
 #include <fstream>
 #include <vector>
--- src/variantcalling/simplerearrangements.h
+++ src/variantcalling/simplerearrangements.h
@@ -1,3 +1,10 @@
+//****************************************************************************
+//* Copyright (c) 2012 Saint-Petersburg Academic University
+//* All Rights Reserved
+//* See file LICENSE for details.
+//****************************************************************************
+
+
 #ifndef SIMPLEREARRANGEMENTS_H
 #define SIMPLEREARRANGEMENTS_H
 
