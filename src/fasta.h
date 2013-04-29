50af20f61ea97d6cb59f9e8976f8e9113f6b6266
fenderglass <fenderglass@gmail.com>
2013-04-29 19:11:28 +0400

    Super fasta perser

--- src/fasta.h
+++ src/fasta.h
@@ -7,7 +7,6 @@
 #ifndef _FASTA_READER_H_
 #define _FASTA_READER_H_
 
-#include <kseq.h>
 #include "common.h"
 
 namespace SyntenyFinder
@@ -17,9 +16,9 @@ namespace SyntenyFinder
 	public:
 		FASTARecord() {}
 		FASTARecord(const std::string & sequence, const std::string & description, size_t id):
-			description_(description), id_(id)
+			description_(description), id_(id), sequence_(sequence)
 		{
-			sequence_.assign(CFancyIterator(sequence.begin(), toupper, ' '), CFancyIterator(sequence.end(), toupper, ' '));
+			//sequence_.assign(CFancyIterator(sequence.begin(), toupper, ' '), CFancyIterator(sequence.end(), toupper, ' '));
 		}
 
 		size_t GetId() const
@@ -48,39 +47,24 @@ namespace SyntenyFinder
 		std::string description_;		
 	};
 
-	//This class provides functionality for reading FASTA files.
-	//Currently this is an OO wrapper around tiny library kseq.h.
-	//It looks little ugly, maybe later it will be replaced with
-	//some other library or written by myself.
-
 	class FASTAReader
 	{
-	public:		
-
-		bool IsOk() const
-		{
-			return fileHandler_ != 0 && !feof(fileHandler_) && !ferror(fileHandler_);
-		}
-
-		explicit FASTAReader(const std::string & fileName): fileHandler_(fopen(fileName.c_str(), "r"))
-		{
-
-		}
-
-		~FASTAReader()
-		{
-			if(fileHandler_ != 0)
-			{
-				fclose(fileHandler_);
-			}
-		}
-
-		size_t GetSequences(std::vector<FASTARecord> & record);
-		
+	public:
+		explicit FASTAReader(const std::string & fileName): 
+			inputStream_(fileName.c_str()),
+			fileName_(fileName) {}
+		size_t 	GetSequences(std::vector<FASTARecord>& record);
+		bool 	IsOk() const;
 	private:
 		DISALLOW_COPY_AND_ASSIGN(FASTAReader);
-		FILE * fileHandler_;
+
+		void ValidateSequence(std::string & sequence);
+		void ValidateHeader(std::string & header);
+
+		std::ifstream inputStream_;
+		std::string fileName_;
 	};
+
 
 	typedef std::vector<FASTARecord> ChrList;
 
