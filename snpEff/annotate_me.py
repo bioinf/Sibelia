5432ea55deea137a15790bd3ec9e1814d88a83dc
allivi <starostinak@gmail.com>
2013-05-19 23:58:40 +0400

    Added search and annotation of mobile elements broken by contigs

--- snpEff/annotate_me.py
+++ snpEff/annotate_me.py
@@ -10,8 +10,8 @@ import os.path
 import re
 import sys
 
-ME_THRESHOLD = 2
-MAX_INDENT_THRESHOLD = 85
+EDGE_THRESHOLD = 10
+ALIGN_THRESHOLD = 30
 
 class Block:
 	def __init__(self, id, strand, start = 0, end = 0):
@@ -61,20 +61,30 @@ class Permutation:
 			return -1
 
 class MobileElement:
-	def __init__(self, permutation_id, location, blocks_seq=None, description="", permutation_name=""):
+	def __init__(self, permutation_id, location, blocks_seq=None, description="", permutation_name="", score=0, last=False, first=False, hit_from=0, hit_to=0, strand=True):
 		self.permutation_id = permutation_id
 		self.blocks_seq = blocks_seq
 		self.location = location
 		self.description = description
 		self.permutation_name = permutation_name
+		self.score = score
+		self.last = last
+		self.first = first
+		self.hit_from = hit_from
+		self.hit_to = hit_to
+		self.strand = strand
 
 	def __str__(self):
 		res = "Description:" + self.description + "\n"
 		res += "Permutation:" + str(self.permutation_id) + " - " + self.permutation_name + "\n"
 		res += "Location:" + str(self.location) + "\n"
-		res += "Blocks:" + ("\n start id = " + str(self.blocks_seq[0]) + ", end id = " + str(self.blocks_seq[1]) if self.blocks_seq else "None")
-#		for block in self.blocks_seq:
-#			res += str(block) + " "
+		res += "Blocks:" + ("\n start id = " + str(self.blocks_seq[0]) + ", end id = " + str(self.blocks_seq[1]) if self.blocks_seq else "None") + "\n"
+		res += "Score:" + str(self.score) + "\n"
+		if self.last:
+			res += "Ending sequence\n"
+		if self.first:
+			res += "Starting sequence\n"
+		res += "Hit from " + str(self.hit_from) + " to " + str(self.hit_to) + ", " + ("plus" if self.strand else "minus")
 		return res
 
 def find_neighbors(permutations, before_me, after_me, before, after):
@@ -114,7 +124,7 @@ def find_difference(mobile_elements, permutations, fout):
 				fout.write("Insert near: " + str(block_id) + " - " + str(permutations[0].permutation[block_id]) + "\n\n")
 
 def blast_sequence(sequence, mobile_elements, permutations):
-	blast_handle = NCBIWWW.qblast("blastn", "nr", sequence, megablast = True, entrez_query = "insert*[Title] OR transpos*[Title]")
+	blast_handle = NCBIWWW.qblast("blastn", "nr", sequence, megablast = True, entrez_query = "insert*[Title] OR transpos*[Title]", service="megablast")
 #	out = open("tmp_res.xml", "w")
 #	out.write(blast_handle.read())
 #	out.close()
@@ -128,19 +138,26 @@ def blast_sequence(sequence, mobile_elements, permutations):
 				continue
 			for aln in record.alignments:
 				for hsp in aln.hsps:
-					insert_blast_res_to_mobile_elements(mobile_elements, permutation, hsp.query_start, hsp.query_start + len(hsp.query), aln.title)
+					insert_blast_res_to_mobile_elements(mobile_elements, permutation, hsp, aln.title, record.query_letters)
 	except: 
 		print "Unknown error occured " + str(sys.exc_info()) + " " + (" on " + record.query if record else "")
 		return
 
-def insert_blast_res_to_mobile_elements(mobile_elements, permutation, query_start, query_end, description):
+def insert_blast_res_to_mobile_elements(mobile_elements, permutation, hsp, description, seq_len):
+	query_start = hsp.query_start
+	query_end = hsp.query_start + len(hsp.query)
 	start_block = next((i for i, block in enumerate(permutation.permutation) if block.start >= query_start), None)
 	end_block = next((len(permutation.permutation) - 1 - i for i, block in enumerate(permutation.permutation[::-1]) 
 		if block.end <= query_end), None)
+	hit_from = hsp.sbjct_start
+	hit_to = hsp.sbjct_start + len(hsp.sbjct) if hsp.frame[1] == 1 else hsp.sbjct_start - len(hsp.sbjct)
+	if (hit_to < hit_from):
+		hit_from = hit_to
+		hit_to = hsp.sbjct_start
 	if start_block is not None and end_block is not None and start_block <= end_block:
-		mobile_elements.append(MobileElement(permutation_id = permutation.id, location = (query_start, query_end), blocks_seq = (start_block, end_block), description = description, permutation_name = permutation.name))
+		mobile_elements.append(MobileElement(permutation_id = permutation.id, location = (query_start, query_end), blocks_seq = (start_block, end_block), description = description, permutation_name = permutation.name, score=hsp.score, first=(query_start < EDGE_THRESHOLD), last=(seq_len - query_end < EDGE_THRESHOLD), hit_from=hit_from, hit_to=hit_to, strand=(hsp.frame[1] == 1)))
 	else:
-		mobile_elements.append(MobileElement(permutation_id = permutation.id, location = (query_start, query_end), description = description, permutation_name = permutation.name))
+		mobile_elements.append(MobileElement(permutation_id = permutation.id, location = (query_start, query_end), description = description, permutation_name = permutation.name, score=hsp.score, first=(query_start < EDGE_THRESHOLD), last=(seq_len - query_end < EDGE_THRESHOLD), hit_from=hit_from, hit_to=hit_to, strand=(hsp.frame[1] == 1)))
 
 def add_block_start_end(permutations, block_id, seq_id, start, end, prev = 0):
 	permutation = next(p for p in permutations if p.name == seq_id)
@@ -183,6 +200,61 @@ def parse_sequences_file(filename, permutations):
 def parse_assembly_file(filename, sequences):
 	for record in SeqIO.parse(filename, "fasta"):
 		sequences.append(record)
+
+def find_pair(me, pairs):
+	res = []
+	for p in pairs.keys():
+		if (p.permutation_id == me.permutation_id):
+			continue
+		if (p.description == me.description and ((
+				p.first == me.first and p.strand and p.strand != me.strand and 
+				abs(me.hit_from - p.hit_from) > ALIGN_THRESHOLD and 
+				abs(me.hit_to - p.hit_to) > ALIGN_THRESHOLD) or (
+				p.first != me.first and not p.first and p.strand == me.strand and 
+				(me.hit_from > p.hit_from + ALIGN_THRESHOLD or me.hit_from >= p.hit_to) and 
+				me.hit_to >= p.hit_to))):
+			res.append(p)
+	return res
+
+def print_broken_contigs(pairs, fout):
+	fout.write("Break candidates\n")
+
+	for key in pairs.keys():
+		for (i, me) in enumerate(pairs[key]):
+			fout.write(key.permutation_name + 
+				(" + " if key.first != me.first else " - ") + 
+				me.permutation_name + "\n")
+			pair_perm = (me.permutation_id, me.first)
+			for in_key in pairs.keys()[i:]:
+				if in_key.permutation_id != key.permutation_id:
+					continue
+				for in_me in pairs[in_key]:
+					if (in_me.permutation_id, in_me.first) == pair_perm: 
+						fout.write(str(in_key.hit_from) + "-" + str(in_key.hit_to) 
+							+ ": " + str(in_key.score) + ", " 
+							+ ("+" if in_key.strand else "-") + ", " 
+							+ ("first" if in_key.first else "last")
+							+ "\t--\t" 
+							+ str(in_me.hit_from) + "-" + str(in_me.hit_to) 
+							+ ": " + str(in_me.score) 
+							+ ("+" if in_me.strand else "-") + ", " 
+							+ ("first" if in_me.first else "last")
+							+ "\n")
+						del in_me
+
+def find_broken_mobile_elements(mobile_elements, fout):
+	pairs = {}
+	
+	for me in mobile_elements:
+		if me.first or me.last:
+			pairs[me] = []
+
+	for me in pairs.keys():
+		pair = find_pair(me, pairs)
+		for p in pair:
+			pairs[p].append(me)
+	
+	print_broken_contigs(pairs, fout)
 
 if (__name__ == "__main__"):
 
@@ -232,5 +304,7 @@ if (__name__ == "__main__"):
 	for me in mobile_elements:
 		fout.write(str(me) + "\n\n")
 	
-	fout.write("\n\nMobile elements insertions:\n\n")
-	find_difference(mobile_elements, permutations, fout)
+	#fout.write("\n\nMobile elements insertions:\n\n")
+	#find_difference(mobile_elements, permutations, fout)
+
+	find_broken_mobile_elements(mobile_elements, fout)
