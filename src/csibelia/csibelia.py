87abed9ca39d6c24eb47edef871df29abe8ba9d8
Ilya Minkin <ilya.minkinen@gmail.com>
2013-06-21 16:07:32 +0400

    Fixed lagan folder name

--- src/csibelia/csibelia.py
+++ src/csibelia/csibelia.py
@@ -16,7 +16,7 @@ UNCOVER = 0
 MINIMUM_CONTEXT_SIZE = 30
 BLOCKS_FILE = 'blocks_sequences.fasta'
 INSTALL_DIR = os.path.dirname(os.path.abspath(__file__))
-LAGAN_DIR = os.path.join(INSTALL_DIR, '.lagan20')
+LAGAN_DIR = os.path.join(INSTALL_DIR, 'lagan')
 os.environ['LAGAN_DIR'] = LAGAN_DIR
 
 def strip_chr_id(chr_id):
