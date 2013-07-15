f4456d6e541b66b3021a4f9b8fb9ad33aafceeea
Ilya Minkin <ilya.minkinen@gmail.com>
2013-07-15 17:32:29 +0400

    Removed debug output

--- src/sibelia.cpp
+++ src/sibelia.cpp
@@ -251,7 +251,6 @@ int main(int argc, char * argv[])
 		std::cout << "Finding synteny blocks and generating the output..." << std::endl;
 		trimK = std::min(trimK, static_cast<int>(minBlockSize.getValue()));
 		size_t lastK = lastKValue.isSet() ? lastKValue.getValue() : std::min(stage.back().first, static_cast<int>(minBlockSize.getValue()));
-		std::cout << lastK << std::endl;
 		finder->GenerateSyntenyBlocks(lastK, trimK, minBlockSize.getValue(), history.back(), sharedOnly.getValue(), PutProgressChr);
 		if(!noPostProcessing)
 		{
